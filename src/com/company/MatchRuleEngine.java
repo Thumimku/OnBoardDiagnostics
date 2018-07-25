9445284860eadaa92f86f4223a8b3ce50284dcb1
Thumimku <thumilan.15@cse.mrt.ac.lk>
2018-07-25 09:49:02 +0530

    Match rule method initiated

--- src/com/company/MatchRuleEngine.java
+++ src/com/company/MatchRuleEngine.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MatchRuleEngine {
-    private String generalRegex;
+    private String generalErrorRegex;
+    private String generalInfoRegex;
     private static   MatchRuleEngine matchRuleEngine;
+    private Pattern pattern;
+    private Matcher matcher;
+    private Boolean parsemode;
 
     private MatchRuleEngine() {
-        generalRegex=new XmlHelper().getGeneralErrorRegEx();
+        parsemode=false;
     }
     public static synchronized MatchRuleEngine getInstance(){
         if(matchRuleEngine==null){
@@ -14,11 +21,46 @@ public class MatchRuleEngine {
         return matchRuleEngine;
     }
 
-    public boolean checkinitialmatch(String testline){
-        if(testline.matches(generalRegex)){
+    private boolean checkInitialErrorMatch(String testline){
+        generalErrorRegex=new XmlHelper().getGeneralErrorRegEx();
+        pattern=Pattern.compile(generalErrorRegex);
+        matcher=pattern.matcher(testline);
+        if(matcher.find()){
             return true;
         }else{
             return false;
+        }
+    }
+    private boolean checkInitialInfoMatch(String testline){
+        generalInfoRegex=new XmlHelper().getGeneralInfoRegEx();
+        pattern=Pattern.compile(generalInfoRegex);
+        matcher=pattern.matcher(testline);
+        if(matcher.find()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void validateTestline(String testline){
+        if (parsemode==true){
+            if(checkInitialInfoMatch(testline)){
+                parsemode=false;
+
+            }else{
+                //parsemode remain true
+                System.out.println(testline);
+            }
+
+
+        }else{
+            if(checkInitialErrorMatch(testline)){
+                parsemode=true;
+                System.out.println(testline);
+
+
+            }else{
+                //parsemode remain false
+            }
         }
     }
 }
